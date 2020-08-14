a85d7cf97c61c072de933ad9d1d2af094beeeae9
Julius <juliusvdw@gmail.com>
2020-08-14 18:46:26 +0200

    Cors added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const passport = require("passport");
+const path = require("path");
+const cors = require("cors");
 const LocalStrategy = require("passport-local").Strategy;
 
 //Bring in local files/modules/middleware
@@ -15,7 +17,9 @@ const watchlist = require("./routes/watchlist");
 const User = require("./models/User");
 
 //config app
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 // Configure passport middleware and session
 app.use(session({ secret: "helloworld" }));
@@ -37,4 +41,9 @@ const PORT = process.env.PORT || 5000;
 //Mount routes
 app.use("/auth", auth);
 app.use("/watchlist", watchlist);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+
 app.listen(PORT, console.log("express server started"));
