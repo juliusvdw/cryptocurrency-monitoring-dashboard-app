e8fad5b801b0c36c862c853ad2f4fc0b36f7b12b
juliusvdw <juliusvdw@gmail.com>
2021-06-29 19:10:40 +0200

    Removed inactive nav for mobile, Created setactivelink func and added onclick event listener to nav links

--- client/src/components/layout/TopNav.js
+++ client/src/components/layout/TopNav.js
@@ -3,50 +3,80 @@ import { Link } from "react-router-dom";
 import NavSearchBox from "./NavSearchBox";
 
 import AuthContext from "../../context/auth/authContext";
+import { set } from "mongoose";
 
 const TopNav = ({ setRegisterModalShow, setLoginModalShow }) => {
   const authContext = useContext(AuthContext);
   const { user, userLogout } = authContext;
 
+  const setActiveLink = (e) => {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    let links = [...navLinks];
+    links.forEach(el => {
+      el.classList.remove('active');
+      e.target.classList.add('active')
+    })
+  }
+
   return (
     <>
-      <nav
-        className="navbar navbar-light d-none d-md-flex  "
-        style={{ background: "#141414" }}
-      >
-        <a
-          className="navbar-brand  text-white d-none d-md-flex"
-          href="#"
-          style={{
-            marginLeft: "70px",
-            fontWeight: "bold",
-            height: "60px",
-            fontSize: "32px",
-          }}
-        >
-          cryptohawk
-        </a>
+    
 
-        <a
-          className="d-md-none mx-auto text-white"
-          style={{
-            marginLeft: "60px",
-            fontWeight: "bold",
-            fontSize: "30px",
-            height: "60px",
-          }}
+        <nav
+          className="navbar navbar-expand-lg "
+          style={navbarStyle}
         >
-          cryptohawk
-        </a>
+          <a
+            className="navbar-brand"
+            href="#"
+            style={{ fontWeight: "bold", fontSize: "30px" }}
+          >
+            Cryptohawk
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon "></span>
+          </button>
 
-        <div className="d-none d-lg-block  mx-auto" style={{ width: "41%" }}>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav  ">
+              <li className="nav-item ">
+                <Link to="/" className="nav-link " href="#" onClick = {setActiveLink}>
+                  Dashboard{" "}
+                </Link>
+              </li>
+              
+              <li className="nav-item">
+                <Link
+                  to="/chart/default"
+                  className="nav-link active "
+                  href="#"
+                  onClick = {setActiveLink}
+                >
+                  Chart
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/allcoins" className="nav-link " href="#" onClick = {setActiveLink}>
+                  Coins
+                </Link>
+              </li>
+            </ul>
+
+            <div className="d-none d-lg-block  mx-auto" style={{ width: "41%" }}>
           <NavSearchBox />
         </div>
+          </div>
 
-        <div className="d-none d-md-block d-lg-none   mx-auto">
-          <NavSearchBox />
-        </div>
-        {user === null ? (
+          {user === null ? (
           <div
             className="nav-buttons ml-auto  d-none d-md-block"
             style={{ marginRight: "50px" }}
@@ -77,69 +107,20 @@ const TopNav = ({ setRegisterModalShow, setLoginModalShow }) => {
             </div>
           </div>
         )}
-      </nav>
-
-      <nav
-        className="navbar navbar-expand-lg navbar-dark d-md-none"
-        style={{ background: "#141414" }}
-      >
-        <a
-          className="navbar-brand text-white"
-          href="#"
-          style={{ fontWeight: "bold", fontSize: "30px" }}
-        >
-          Cryptohawk
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon "></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto text-white">
-            <li className="nav-item active">
-              <Link to="/" className="nav-link text-white" href="#">
-                Home{" "}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/news" className="nav-link text-white" href="#">
-                News
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link
-                to="/chart/default"
-                className="nav-link text-white"
-                href="#"
-              >
-                Chart
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/allcoins" className="nav-link text-white" href="#">
-                All Coins
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        </nav>
     </>
   );
 };
 
 const btnStyle = {
-  color: "white",
+  color: "#FFFFF",
   fontWeight: "bold",
   fontSize: "13.5px",
   borderWidth: "1.2px",
 };
+
+const navbarStyle = {
+  backgroundColor: '#FFFFF'
+  
+}
 export default TopNav;
