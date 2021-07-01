f16a43b732757cda240f922984ca4f6dec3e912f
juliusvdw <juliusvdw@gmail.com>
2021-07-01 18:09:54 +0200

    Watchlistitem styles updated for facelift, removed old unused code

--- client/src/components/watchlist/WatchlistItem.js
+++ client/src/components/watchlist/WatchlistItem.js
@@ -20,7 +20,7 @@ const WatchlistItem = (props) => {
   //modify the data for display purposes
   let percentColour;
   percentChange > 0
-    ? (percentColour = "lightgreen")
+    ? (percentColour = "#4e9d66")
     : (percentColour = "#F14848");
   percentChange > 0
     ? (percentChange = percentChange.toString().slice(0, 4))
@@ -34,102 +34,142 @@ const WatchlistItem = (props) => {
   );
 
   return (
-    <Link
-      to={`/coin/${id}`}
-      style={{ textDecoration: "none", color: "inherit" }}
-    >
-      <div
-        className="card  "
-        id="watchlist-card"
-        style={{
-          borderRadius: "5px",
-          backgroundColor: "#2D2D2D",
-          marginBottom: "20px",
-        }}
-      >
-        <div className="card-body">
-          <div className="row  mx-auto" style={{ marginBottom: "22px" }}>
-            <span style={{ maxHeight: "20px", maxWidth: "20px" }}>
-              <img className="img-fluid d-none d-lg-block" src={`${image}`} />
-            </span>
-            <span
-              style={{
-                fontSize: "1.3rem",
-                fontWeight: "bold",
-                paddingLeft: "15px",
-              }}
-            >
-              {id}
-            </span>
+    <>
+              <div className = 'watchlist-item-container' style = {itemContainerStyle}>
+                <div className = 'coin-id-container' style = {coinIdStyle}>
+                <span className = 'watchlist-img pl-3'> <img src = {`${image}`} style = {imageStyle}></img> </span> <span className = 'pl-3' >{id}</span>
+                </div>
 
-            <span
-              className="ml-auto"
-              style={{
-                fontSize: "1.3rem",
-                fontWeight: "bold",
-                color: percentColour,
-              }}
-            >
-              {`${percentChange}%`}
-            </span>
-          </div>
-          <div
-            className="row w-90 mx-auto"
-            style={{ fontSize: "0.8rem", borderBottom: "1px solid #5f5f5f" }}
-          >
-            <span className="pb-1"> Daily low:</span>
-            <span className="ml-auto">$ {low}</span>
-          </div>
-          <div
-            className="row w-90 mx-auto"
-            style={{ fontSize: "0.8rem", borderBottom: "1px solid #5f5f5f" }}
-          >
-            <span className="pt-1 pb-1"> Daily High:</span>
-            <span className="ml-auto pt-1">$ {high}</span>
-          </div>
-          <div className="row w-90 mx-auto" style={{ fontSize: "0.8rem" }}>
-            {" "}
-            <Link to="#">
-              <span
-                className="btn btn-outline-danger mt-3  card-btn mr-1"
-                style={{
-                  height: "30px",
-                  width: "45px",
-                  fontSize: "0.7rem",
-                  color: "white",
-                  visibility: "hidden",
-                  borderWidth: "1.2px",
-                }}
-                onClick={() => watchlistDelete(id)}
-              >
-                <strong>
-                  <i className="fas fa-trash"></i>
-                </strong>
-              </span>
-            </Link>
-            <Link to={`/chart/${symbol}usd`}>
-              <span
-                className="btn btn-outline-primary mt-3  card-btn"
-                style={{
-                  height: "30px",
-                  width: "70px",
-                  fontSize: "0.7rem",
-                  color: "white",
-                  visibility: "hidden",
-                  borderWidth: "1.2px",
-                }}
-              >
-                <strong>Chart</strong>
-              </span>
-            </Link>
-            <span className="ml-auto pt-3">
-              <strong style={{ fontSize: "1.2rem" }}>$ {price}</strong>
-            </span>
-          </div>
-        </div>
-      </div>
-    </Link>
+                <div className = 'coin-price-container' style = {infoStyle}>
+                  <span >$ {price}</span>
+                </div>
+
+                <div className = 'coin-price-container' style = {infoStyle}>
+                  <span >$ {high}</span>
+                </div>
+
+                <div className = 'coin-price-container' style = {infoStyle}>
+                  <span style = {{color: `${percentColour}`, fontWeight:'bold'}}>{percentChange} %</span>
+                </div>
+              
+              
+              </div>
+            
+         </>
+    
   );
 };
 
+const itemContainerStyle = {
+  display: 'flex',
+  height: '80px',
+  borderBottom: '1px solid #F0F0F0',
+  alignItems:'center',
+  color: '#021E69'
+}
+
+const imageStyle = {
+  height:'40px',
+  width :'40px'
+}
+
+const coinIdStyle = {
+  fontWeight:'450',
+  flex: '1'
+}
+
+const infoStyle = {
+  fontSize: '14px',
+  paddingLeft : '70px',
+  flex : '1'
+}
+
+const percentStyle = {
+}
+
 export default WatchlistItem;
+
+// <div
+// className="card  "
+// id="watchlist-card"
+// style={{
+//   borderRadius: "5px",
+//   backgroundColor: "#2D2D2D",
+//   marginBottom: "20px",
+// }}
+// >
+{/* <div className="card-body">
+  <div className="row  mx-auto" style={{ marginBottom: "22px" }}>
+    <span style={{ maxHeight: "20px", maxWidth: "20px" }}>
+      <img className="img-fluid d-none d-lg-block" src={`${image}`} />
+    </span>
+    <span
+      style={{
+        fontSize: "1.3rem",
+        fontWeight: "bold",
+        paddingLeft: "15px",
+      }}
+    >
+      {id}
+    </span>
+
+    <span
+      className="ml-auto"
+      style={{
+        fontSize: "1.3rem",
+        fontWeight: "bold",
+        color: percentColour,
+      }}
+    >
+      {`${percentChange}%`}
+    </span>
+  </div>
+  <div
+    className="row w-90 mx-auto"
+    style={{ fontSize: "0.8rem", borderBottom: "1px solid #5f5f5f" }}
+  >
+    <span className="pb-1"> Daily low:</span>
+    <span className="ml-auto">$ {low}</span>
+  </div>
+  <div
+    className="row w-90 mx-auto"
+    style={{ fontSize: "0.8rem", borderBottom: "1px solid #5f5f5f" }}
+  >
+    <span className="pt-1 pb-1"> Daily High:</span>
+    <span className="ml-auto pt-1">$ {high}</span>
+  </div>
+  <div className="row w-90 mx-auto" style={{ fontSize: "0.8rem" }}>
+    {" "}
+    <Link to="#">
+      <span
+        className="btn btn-outline-danger mt-3  card-btn mr-1"
+        style={{
+          height: "30px",
+          width: "45px",
+          fontSize: "0.7rem",
+          color: "white",
+          visibility: "hidden",
+          borderWidth: "1.2px",
+        }}
+        onClick={() => watchlistDelete(id)}
+      >
+        <strong>
+          <i className="fas fa-trash"></i>
+        </strong>
+      </span>
+    </Link>
+    <Link to={`/chart/${symbol}usd`}>
+      <span
+        className="btn btn-outline-primary mt-3  card-btn"
+        style={{
+          height: "30px",
+          width: "70px",
+          fontSize: "0.7rem",
+          color: "white",
+          visibility: "hidden",
+          borderWidth: "1.2px",
+        }}
+      >
+        <strong>Chart</strong>
+      </span> */}
+
