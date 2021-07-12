7700eaf1424fcde886fe90d6cba23b324771b489
juliusvdw <juliusvdw@gmail.com>
2021-07-12 17:01:25 +0200

    Removed old code, new styling for each coin item. Placeholder values for now

--- client/src/components/allCoins/AllCoinsItem.js
+++ client/src/components/allCoins/AllCoinsItem.js
@@ -18,57 +18,126 @@ const AllCoinsItem = (props) => {
   } = props.coin;
 
   return (
-    <div
-      className="row w-100 pt-3 allcoin-item-container"
-      style={{ minHeight: "60px", borderBottom: "1px solid #555555" }}
-    >
-      <div className="col-lg-3 col-md-4 d-flex flex-row">
-        <img
-          className=" img-fluid "
-          src={`${image}`}
-          style={{ height: "25px", width: "25px" }}
-        />
-        <span className="pl-4">
-          <Link
-            className="coin-link"
-            to={`coin/${id}`}
-            style={{ textDecoration: "none", color: "#ffff" }}
+    <div className = 'allcoins-item-container' style = {containerStyle}>
+
+      <div className = 'coin-id-container' style = {idContainerStyle}>
+        <span><img src = {image} style = {imageStyle}/></span>
+        <span className = 'pl-3' style = {idStyle}>{id.replace(id.charAt(0), id.charAt(0).toUpperCase())}</span>
+        <span style = {symbolStyle}>{symbol.toUpperCase()}</span>
+        </div>
+
+        <div className = 'price-container' style = {infoContainerStyle}>
+          <span style = {priceStyle}>$ {current_price}</span>
+        </div>
+
+            <div
+                 style={{
+              color: price_change_percentage_24h > 0 ? "#4E9D66" : "#F14848", flex:'1', fontWeight:'bold',fontSize:'15px'
+            }}
           >
-            {id.replace(id.charAt(0), id.charAt(0).toUpperCase())}{" "}
-          </Link>
-        </span>
-      </div>
+            {price_change_percentage_24h && price_change_percentage_24h < 0
+              ? String(price_change_percentage_24h).slice(1, 5)
+              : String(price_change_percentage_24h).slice(0, 4)}{" "}
+            %
+          </div>
 
-      <div className="col-lg-2 col-md-3 mt-1 ">$ {current_price}</div>
+        <div className = 'price-container' style = {infoContainerStyle}>
+          <span style = {priceStyle}>$ {current_price}</span>
+        </div>
 
-      <div
-        className="col-lg-1 col-md-2 mt-1 mb-2"
-        style={{
-          color: price_change_percentage_24h > 0 ? "lightgreen" : "#F14848",
-        }}
-      >
-        {price_change_percentage_24h && price_change_percentage_24h < 0
-          ? String(price_change_percentage_24h).slice(1, 5)
-          : String(price_change_percentage_24h).slice(0, 4)}{" "}
-        %
-      </div>
+        <div className = 'price-container' style = {infoContainerStyle}>
+          <span style = {priceStyle}>$ {current_price}</span>
+        </div>
 
-      <div className="col-lg-3 col-md-3 d-none d-lg-block">
-        $ {total_volume}
-      </div>
-
-      <div className="col-lg-2 col-md-2 d-none d-lg-block">$ {market_cap}</div>
-
-      <div className="col-lg-1 col-md-2 d-none d-md-block">
-        <span onClick={() => watchlistAdd(id)} className="watchlist-add-btn">
-          <i
-            className="fas fa-plus "
-            style={{ fontSize: "1.6rem", color: "lightgreen" }}
-          />
-        </span>
-      </div>
     </div>
+   
   );
 };
 
+const containerStyle = {
+  display:'flex',
+  borderBottom:'1px solid rgb(240,240,240)',
+  height:'85px',
+  alignItems:'center',
+  color:'#021E69'
+}
+
+const imageStyle = {
+  height:'45px',
+  width:'45px'
+}
+
+const idContainerStyle = {
+  flex:'2'
+}
+const idStyle = {
+  fontWeight:'450'
+}
+
+const symbolStyle = {
+  fontSize:'12px',
+  color:'#A5A5A5',
+  marginLeft :'10px',
+  fontWeight:'bold'
+}
+
+const infoContainerStyle = {
+  flex:'1',
+  fontSize:'15px'
+}
+const priceStyle = {
+}
+
 export default AllCoinsItem;
+
+
+{/* <div
+className="row  pt-3 allcoin-item-container"
+style={{ minHeight: "60px", borderBottom: "1px solid #555555" }}
+>
+<div className="col-lg-3 col-md-4 d-flex flex-row">
+  <img
+    className=" img-fluid "
+    src={`${image}`}
+    style={{ height: "25px", width: "25px" }}
+  />
+  <span className="pl-4">
+    <Link
+      className="coin-link"
+      to={`coin/${id}`}
+      style={{ textDecoration: "none", color: "#ffff" }}
+    >
+      {id.replace(id.charAt(0), id.charAt(0).toUpperCase())}{" "}
+    </Link>
+  </span>
+</div>
+
+<div className="col-lg-2 col-md-3 mt-1 ">$ {current_price}</div>
+
+<div
+  className="col-lg-1 col-md-2 mt-1 mb-2"
+  style={{
+    color: price_change_percentage_24h > 0 ? "lightgreen" : "#F14848",
+  }}
+>
+  {price_change_percentage_24h && price_change_percentage_24h < 0
+    ? String(price_change_percentage_24h).slice(1, 5)
+    : String(price_change_percentage_24h).slice(0, 4)}{" "}
+  %
+</div>
+
+<div className="col-lg-3 col-md-3 d-none d-lg-block">
+  $ {total_volume}
+</div>
+
+<div className="col-lg-2 col-md-2 d-none d-lg-block">$ {market_cap}</div>
+
+<div className="col-lg-1 col-md-2 d-none d-md-block">
+  <span onClick={() => watchlistAdd(id)} className="watchlist-add-btn">
+    <i
+      className="fas fa-plus "
+      style={{ fontSize: "1.6rem", color: "lightgreen" }}
+    />
+  </span>
+</div>
+</div> */}
