9706d5b4a93fc5f7c220cfd0197e58220628f3f3
juliusvdw <juliusvdw@gmail.com>
2021-07-27 12:52:21 +0200

    Added up and down arrow next to percentchange + logic to determine which arrow to display based on positive or negative percent change

--- client/src/components/coin/CoinLeftCard.js
+++ client/src/components/coin/CoinLeftCard.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import {GraphUp, PlusCircle, ArrowUpShort, ArrowDownShort} from 'react-bootstrap-icons';
 
 import CoinFeedContext from "../../context/coinFeed/coinFeedContext";
 
@@ -36,12 +37,56 @@ const CoinStats = () => {
       low_24h,
       atl,
       market_cap_rank,
+      current_price,
+      
     } = market_data;
+
+    let percentChange = market_data.price_change_percentage_24h_in_currency.usd;
+
+   const {
+     symbol,
+     id
+   } = stats;
+
+   const image = stats.image.small;
+
+   
+
+   //Determine which arrow to display based on price change percentage 
+  let arrow; 
+  percentChange > 0 
+  ? arrow = <ArrowUpShort />
+  : arrow = <ArrowDownShort />
+
+    //modify the data for display purposes, percentColor + price length
+  let percentColour;
+  percentChange > 0
+    ? (percentColour = "#4e9d66")
+    : (percentColour = "#F14848");
+  percentChange > 0
+    ? (percentChange = percentChange.toString().slice(0, 4))
+    : (percentChange = percentChange.toString().slice(1, 5));
+
+  const price = current_price.usd.toString().slice(0, 7);
+
+  
 
     //Output content if coin stats have loaded
     statsOutput = (
       <>
-        
+       <div  style = {topRowStyle}>
+            <img src = {`${image}`} style = {imageStyle} />
+            <span className = 'pl-3' style = {mainTextStyle}>{id.replace(id.charAt(0), id.charAt(0).toUpperCase())}</span>
+            <span className = 'mt-1' style = {symbolStyle}>{`(${symbol.toUpperCase()})`}</span>
+
+            <span className = 'ml-4' style = {graphIconStyle}><GraphUp className = 'icon'/></span>
+            <span className = 'ml-auto' style = {addIconStyle}><PlusCircle className = 'icon'/></span>
+      </div>
+
+      <div style = {priceBoxStyle}>
+         <div style = {priceStyle} >$ {price}</div>
+         <div style = {{ color: percentColour}}  className = 'mt-3'><span style = {arrowStyle}>{arrow}</span> {percentChange} %</div>
+      </div>
       </>
     );
   }
@@ -53,6 +98,67 @@ const CoinStats = () => {
   );
 };
 
+const topRowStyle = {
+  display:'flex',
+  alignItems:'center'
+}
+
+const imageStyle = {
+  height:'45px',
+  width:'45px'
+}
+
+const symbolStyle = {
+  fontSize:'12px',
+  color:'#A5A5A5',
+  marginLeft :'10px',
+  fontWeight:'bold'
+}
+
+const mainTextStyle = {
+  color:'#021E69',
+  fontSize:'20px',
+  fontWeight:'450'
+}
+
+const priceStyle = {
+  textAlign:'center',
+  color:'#021E69'
+}
+
+const priceBoxStyle = {
+  height:'200px',
+  display:'flex',
+  flexDirection:'column',
+  alignItems:'center',
+  justifyContent:'center',
+  marginTop:'20px',
+  fontSize:'46px',
+  fontWeight: '450'
+}
+
+const graphIconStyle = {
+  color:'#374FC9',
+  fontSize:'18px'
+}
+
+const addIconStyle = {
+  color:'#374FC9',
+  fontSize:'18px'
+}
+
+const arrowStyle = {
+  fontSize:'42px'
+}
+
+
 
 
 export default CoinStats;
+
+
+{/* <div className = 'd-flex ' style = {cardFlexRow}>
+            <img src = {`${image}`} style = {imageStyle} />
+            <span className = 'pl-3' style = {cardMainText}>{id}</span>
+            <span className = 'mt-1' style = {symbolStyle}>{`(${symbol.toUpperCase()})`}</span>
+          </div> */}
