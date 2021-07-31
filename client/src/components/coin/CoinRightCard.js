12eb1f3f83d89281323ff809ba8b64cbab1d23f0
juliusvdw <juliusvdw@gmail.com>
2021-07-31 12:30:52 +0200

    Variables names changed for easier access + dynamic styles created for up + down percentage changes

--- client/src/components/coin/CoinRightCard.js
+++ client/src/components/coin/CoinRightCard.js
@@ -20,15 +20,35 @@ const CoinRightCard = () => {
             price_change_percentage_30d
          } = market_data
 
+         //Rename variables for easier access
+         const oneYearChange =  price_change_percentage_1y,
+               dailyChange = price_change_percentage_24h,
+               weeklyChange = price_change_percentage_7d,
+               twoWeekChange = price_change_percentage_14d,
+               monthlyChange = price_change_percentage_30d
+
+         //Classify classes to determine the color of the percentage displayed
+         const styleUp = {
+             color:'green',
+             fontSize:'15px',
+             fontWeight:'450'
+         }
+         const styleDown = {
+             color:'red',
+             fontSize:'15px',
+             fontWeight:'450'
+         }
+         
+
 
         return (
             <>
             <div style = {headingStyle}> <h6>Price Change Percentage</h6></div>
-            <div style = {rowStyle}><span style = {labelStyle}>24H Change</span> <span style = {percentStyle} className = 'ml-auto'> {price_change_percentage_24h}</span></div>
-            <div style = {rowStyle}><span style = {labelStyle}>7D Change</span><span style = {percentStyle} className = 'ml-auto'>{price_change_percentage_7d}</span></div>
-            <div style = {rowStyle}><span style = {labelStyle}>14D Change</span><span style = {percentStyle} className = 'ml-auto'>{price_change_percentage_14d}</span></div>
-            <div style = {rowStyle}><span style = {labelStyle}>30D Change</span><span style = {percentStyle} className = 'ml-auto'>{price_change_percentage_30d}</span></div>
-            <div style = {rowStyle}><span style = {labelStyle}>1Y Change</span> <span style = {percentStyle} className = 'ml-auto'> {price_change_percentage_1y}</span></div>
+            <div style = {rowStyle}><span style = {labelStyle}>24H Change</span> <span style = {dailyChange > 0 ? styleUp : styleDown} className = 'ml-auto'> {dailyChange.toString().slice(0,5)} %</span></div>
+            <div style = {rowStyle}><span style = {labelStyle}>7D Change</span><span style = {weeklyChange > 0 ? styleUp : styleDown} className = 'ml-auto'>{weeklyChange.toString().slice(0,5)} %</span></div>
+            <div style = {rowStyle}><span style = {labelStyle}>14D Change</span><span style = {twoWeekChange > 0 ? styleUp : styleDown} className = 'ml-auto'>{twoWeekChange.toString().slice(0,5)} %</span></div>
+            <div style = {rowStyle}><span style = {labelStyle}>30D Change</span><span style = {monthlyChange > 0 ? styleUp : styleDown} className = 'ml-auto'>{monthlyChange.toString().slice(0,5)} %</span></div>
+            <div style = {rowStyle}><span style = {labelStyle}>1Y Change</span> <span style = {oneYearChange > 0 ? styleUp : styleDown} className = 'ml-auto'> {oneYearChange.toString().slice(0,5)} %</span></div>
 
             </>
         )
@@ -65,8 +85,5 @@ const headingStyle = {
 
 
 
-const percentStyle = {
-
-}
 
 export default CoinRightCard
